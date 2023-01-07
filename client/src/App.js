47785d52cb9c136989b91aa96ba8105eb64269c6
himanshu9999git <hchimanshu9999@gmail.com>
2023-01-08 02:48:24 +0530

    Set-up Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [title, setTitle] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="App">
+      <div className="AddPassWords">
+        <input
+          type="text"
+          placeholder="Eg. Facebook"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Eg. password123"
+          onChange={(event) => {
+            setPassword(event.target.value);
+            console.log(password);
+          }}
+        />
+        <button className="btn">Add Password</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
